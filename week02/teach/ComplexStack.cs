3538e5e7c7709d33cd224e6b6a3033dacdd2b5f6
richardlyonheart <brendenlyon907@gmail.com>
2025-07-10 17:44:43 -0400

    team activity

--- week02/teach/ComplexStack.cs
+++ week02/teach/ComplexStack.cs
@@ -1,9 +1,10 @@
 ﻿public static class ComplexStack {
     public static bool DoSomethingComplicated(string line) {
+        //line = ?(dffks){sdfsdf}[arearfe]
         var stack = new Stack<char>();
         foreach (var item in line) {
             if (item is '(' or '[' or '{') {
-                stack.Push(item);
+                stack.Push(item);//stack [{(
             }
             else if (item is ')') {
                 if (stack.Count == 0 || stack.Pop() != '(')
--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -1,9 +1,13 @@
-﻿/// <summary>
+﻿using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
+
+/// <summary>
 /// Maintain a Customer Service Queue.  Allows new customers to be 
 /// added and allows customers to be serviced.
 /// </summary>
 public class CustomerService {
-    public static void Run() {
+    public static void Run()
+    {
         // Example code to see what's in the customer service queue:
         // var cs = new CustomerService(10);
         // Console.WriteLine(cs);
@@ -11,24 +15,58 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: add custoner and serve
+        // Expected Result: display customer added
         Console.WriteLine("Test 1");
+        var service = new CustomerService(4);
+        service.AddNewCustomer();
+        service.ServeCustomer();
 
-        // Defect(s) Found: 
+
+        // Defect(s) Found: serve custiner was deleting before adding to list
 
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: can i derve more than 1 customer in order
+        // Expected Result: show customers in order
         Console.WriteLine("Test 2");
+        service = new CustomerService(5);
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        Console.WriteLine($"before serving order {service}");
+        service.ServeCustomer();
+        service.ServeCustomer();
+        Console.WriteLine($"after serving. {service}");
 
-        // Defect(s) Found: 
+
+
+        // Defect(s) Found: worked as espected
 
         Console.WriteLine("=================");
 
         // Add more Test Cases As Needed Below
+
+        Console.WriteLine("-------------------------");
+        //test 3
+        //scenario if que is full show an error messsage
+        //expected result error message
+        Console.WriteLine("test 3");
+        service = new CustomerService(1);
+        service.AddNewCustomer();
+        service.AddNewCustomer();
+        Console.WriteLine($"{service}");
+        //defect found no error message, allowed to add beyond queue
+
+        Console.WriteLine("++++++++++++++++++++++");
+        //test 4
+        // scenario can I serve a no customer
+        //expected result not able to serve a customer that hasnt been created
+        Console.WriteLine("test 4");
+        service = new CustomerService(1);
+        service.ServeCustomer();
+        Console.WriteLine($"{service}");
+        //defect: error but no message in code to let me know why code failed, added a check in serve customers to ensure there was a queue
     }
 
     private readonly List<Customer> _queue = new();
@@ -67,7 +105,7 @@ public class CustomerService {
     /// </summary>
     private void AddNewCustomer() {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count >= _maxSize) {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
@@ -88,9 +126,15 @@ public class CustomerService {
     /// Dequeue the next customer and display the information.
     /// </summary>
     private void ServeCustomer() {
-        _queue.RemoveAt(0);
-        var customer = _queue[0];
-        Console.WriteLine(customer);
+        if (_queue.Count <= 0)
+        {
+            Console.WriteLine("No customers");
+        }
+        else {
+            var customer = _queue[0];
+            _queue.RemoveAt(0);
+            Console.WriteLine(customer);
+        }
     }
 
     /// <summary>
